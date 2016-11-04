85a8e0d1ae24b1390d7f6a4e177125a30b46392a
Anne Belka <antastic461@gmail.com>
2016-11-04 11:12:47 -0700

    add separate functions to find added cost for size and toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,38 @@
 //business logic
-function Pizza(size, toppings) {
+function Pizza(size, toppings, price) {
   this.pizzaSize =size;
   this.toppings = toppings;
   this.price = 10;
 }
 
-Pizza.prototype.findPrice = function() {
-  if(this.toppings.length > 4) {
-    this.price += 8;
-  }else if (this.toppings.length > 3) {
-    this.price += 4;
-  }else if (this.toppings.length > 2) {
-    this.price += 2;
-  } else if (this.pizzaSize === "Small 12 inch") {
-    this.price += 2;
+Pizza.prototype.findPriceBySize = function() {
+  if(this.pizzaSize === "Personal 8 inch") {
+    return this.price;
+  } else if(this.pizzaSize === "Small 12 inch") {
+      this.price += 2;
   }else if (this.pizzaSize === "Medium 16 inch") {
     this.price += 4;
-  }else if (this.pizzaSize === "large 20 inch") {
+  }else if (this.pizzaSize === "Large 20 inch") {
     this.price += 8;
-  } else{
+  } else {
     alert("You must choose a size!")
   } return this.price;
 };
+
+Pizza.prototype.findPriceByTopping = function(newPrice) {
+  if(this.toppings.length === 5) {
+    newPrice += 10;
+  }else if (this.toppings.length === 4) {
+    newPrice += 8;
+  }else if (this.toppings.length === 3) {
+    newPrice += 6;
+  }else if (this.toppings.length === 2) {
+      newPrice += 4;
+  }else if (this.toppings.length === 1) {
+      newPrice += 2;
+  } this.price = newPrice;
+    return this.price;
+}
 
 Pizza.prototype.youOrdered = function() {
   return "A " + this.pizzaSize + " pizza with " + this.toppings + ".<br> Your price will be $" + this.price + " at the door. Don't forget to tip your driver!"
@@ -36,14 +47,15 @@ $(function() {
     var size = $("#pizzaSize").val();
     var toppings = [];
     var newPizza = new Pizza (size, toppings);
-    var newPrice = newPizza.findPrice();
-     $("input:checkbox[name=toppings]:checked").each(function(){
+    $("input:checkbox[name=toppings]:checked").each(function(){
       toppings.push($(this).val());
     });
+    var newPrice = newPizza.findPriceBySize();
+    finalPrice = newPizza.findPriceByTopping(newPrice);
+    newPizza = new Pizza (size, toppings, finalPrice);
+    console.log(finalPrice);
+    console.log(newPizza.price);
     $(".pizzaOrdered").show();
     $("ul#result").append("<li>" + newPizza.youOrdered() + "</li>");
-    console.log(newPizza.pizzaSize);
-
-    alert(newPrice);
   });
 });
